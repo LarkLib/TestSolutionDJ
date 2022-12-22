b18c24c397bb3b9410e138d7b083626c16252a40
admin <admin@DESKTOP-VP94FCM>
2022-12-22 15:47:59 +0800

    reorg code

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestClassLibrary;
+
+namespace TestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var testLibrary = new TestClass();
+            var msg = testLibrary.ReciveData();
+        }
+    }
+}
