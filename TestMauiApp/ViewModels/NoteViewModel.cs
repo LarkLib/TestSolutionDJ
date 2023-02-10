2340d04ad6cf1a4a80c7bc2e6fc7a60cf8a7f982
admin <admin@DESKTOP-VP94FCM>
2023-02-10 16:42:46 +0800

    Tutorial: Use the MVVM toolkit to update the Notes app - .NET MAUI

---
+++ TestMauiApp/ViewModels/NoteViewModel.cs
@@ -0,0 +1,82 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+
+namespace TestMauiApp.ViewModels
+{
+    internal class NoteViewModel : ObservableObject, IQueryAttributable
+    {
+        private Models.Note _note;
+
+        public ICommand SaveCommand { get; private set; }
+        public ICommand DeleteCommand { get; private set; }
+        public string Text
+        {
+            get => _note.Text;
+            set
+            {
+                if (_note.Text != value)
+                {
+                    _note.Text = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public DateTime Date => _note.Date;
+        public string Identifier => _note.Filename;
+
+        public NoteViewModel()
+        {
+            _note = new Models.Note();
+            SaveCommand = new AsyncRelayCommand<Button>(Save);
+            DeleteCommand = new AsyncRelayCommand(Delete);
+        }
+
+        public NoteViewModel(Models.Note note)
+        {
+            _note = note;
+            SaveCommand = new AsyncRelayCommand<Button>(Save);
+            DeleteCommand = new AsyncRelayCommand(Delete);
+        }
+
+        private async Task Save(object sender)
+        {
+            if (_note == null || sender is not Button || ((Button)sender).Text.Equals("cancel", StringComparison.InvariantCultureIgnoreCase))
+            {
+                await Shell.Current.GoToAsync("..");
+                return;
+            }
+
+            _note.Date = DateTime.Now;
+            _note.Save();
+            await Shell.Current.GoToAsync($"..?saved={_note.Filename}");
+        }
+
+        private async Task Delete()
+        {
+            _note.Delete();
+            await Shell.Current.GoToAsync($"..?deleted={_note.Filename}");
+        }
+
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("load"))
+            {
+                _note = Models.Note.Load(query["load"].ToString());
+                RefreshProperties();
+            }
+        }
+
+        public void Reload()
+        {
+            _note = Models.Note.Load(_note.Filename);
+            RefreshProperties();
+        }
+
+        private void RefreshProperties()
+        {
+            OnPropertyChanged(nameof(Text));
+            OnPropertyChanged(nameof(Date));
+        }
+    }
+}
